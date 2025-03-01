c90c3f7d1c6a8c0fc06b19d2ebf1ff668b242364
fishbraised <fishbraised@gmail.com>
2025-03-01 22:48:53 +0800

    Login fetch request issue pending.

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -12,29 +12,78 @@ import {
   LoginSpan,
 } from "./StyledComponents";
 
-// onSubmit={}
+import { Component } from "react";
 
-const Login = () => (
-  <LoginContainer>
-    <LoginContent>
-      <LoginForm>
-        <WebsiteLogo src="https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-dark-theme-img.png" />
-        <LoginInput type="text" placeholder="Email" />
-        <LoginInput type="text" placeholder="Password" />
+class Login extends Component {
+  state = { email: "", password: "" };
 
-        <CheckboxContainer>
-          <CheckboxInput type="checkbox" />
-          <CheckboxParagraph>Show Password</CheckboxParagraph>
-        </CheckboxContainer>
+  submitLogin = async (event) => {
+    event.preventDefault();
 
-        <LoginButton type="submit">Login</LoginButton>
-        <LoginParagraph>
-          {`Dont have an account?`}
-          <LoginSpan>{`Sign up, it's free!`}</LoginSpan>
-        </LoginParagraph>
-      </LoginForm>
-    </LoginContent>
-  </LoginContainer>
-);
+    const URL = "https://apis.ccbp.in/login";
+    const { email, password } = this.state;
+
+    const userDetails = {
+      email,
+      password,
+    };
+
+    const options = {
+      method: "POST",
+      body: JSON.stringify(userDetails),
+    };
+
+    const response = await fetch(URL, options);
+    const data = await response.json();
+
+    console.log("response: ", response);
+    console.log("data: ", data);
+  };
+
+  updateEmailInput = (event) => {
+    this.setState({ email: event.target.value });
+  };
+
+  updatePasswordInput = (event) => {
+    this.setState({ password: event.target.value });
+  };
+
+  render() {
+    const { email, password } = this.state;
+
+    return (
+      <LoginContainer>
+        <LoginContent>
+          <LoginForm onSubmit={this.submitLogin}>
+            <WebsiteLogo src="https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-dark-theme-img.png" />
+            <LoginInput
+              onChange={this.updateEmailInput}
+              value={email}
+              type="text"
+              placeholder="Email"
+            />
+            <LoginInput
+              onChange={this.updatePasswordInput}
+              value={password}
+              type="text"
+              placeholder="Password"
+            />
+
+            <CheckboxContainer>
+              <CheckboxInput type="checkbox" />
+              <CheckboxParagraph>Show Password</CheckboxParagraph>
+            </CheckboxContainer>
+
+            <LoginButton type="submit">Login</LoginButton>
+            <LoginParagraph>
+              {`Dont have an account?`}
+              <LoginSpan>{`Sign up, it's free!`}</LoginSpan>
+            </LoginParagraph>
+          </LoginForm>
+        </LoginContent>
+      </LoginContainer>
+    );
+  }
+}
 
 export default Login;
